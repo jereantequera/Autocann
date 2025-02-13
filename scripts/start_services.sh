b02f13573f28fc5450bb0c71f4592f01e4444fc7
jereantequera <jereantequera@gmail.com>
2025-02-13 17:18:18 -0300

    Update start_services.sh: Modify virtual environment activation path

--- scripts/start_services.sh
+++ scripts/start_services.sh
@@ -3,8 +3,7 @@
 FECHA=$(date +'%Y-%m-%d')
 # Iniciar el contenedor de Redis
 docker start redis-stack
-cd /home/autocann/Autocann
-source autocann/bin/activate
 cd /home/autocann/Autocann/scripts
+source venv/bin/activate
 python fix-vpd.py early_veg 2>> "/home/autocann/Autocann/logs/errors_vpd_$FECHA.log"
 
